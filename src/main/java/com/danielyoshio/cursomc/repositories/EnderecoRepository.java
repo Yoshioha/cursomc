a635623621226c852bd4b817eea94237aec11fb8
Daniel Yoshio <danielyoshio2013@gmail.com>
2019-11-18 09:36:14 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/danielyoshio/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.danielyoshio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danielyoshio.cursomc.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
