dfc981643391eef1868920f54c6a75d769fd972a
Daniel Yoshio <danielyoshio2013@gmail.com>
2019-12-16 14:28:45 -0300

    Endpoint /pedidos/{id} disponiblizado

---
+++ src/main/java/com/danielyoshio/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.danielyoshio.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielyoshio.cursomc.domain.Pedido;
+import com.danielyoshio.cursomc.domain.Cliente;
+import com.danielyoshio.cursomc.repositories.PedidoRepository;
+import com.danielyoshio.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
