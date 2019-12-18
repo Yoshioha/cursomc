b6e2d3aeffd7aa34c479adf7025c963560cf3e6f
Daniel Yoshio <danielyoshio2013@gmail.com>
2019-12-18 13:26:44 -0300

    Atualizando Categoria com PUT e outros pequenos ajustes

--- src/main/java/com/danielyoshio/cursomc/services/PedidoService.java
+++ src/main/java/com/danielyoshio/cursomc/services/PedidoService.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.danielyoshio.cursomc.domain.Pedido;
 import com.danielyoshio.cursomc.domain.Cliente;
+import com.danielyoshio.cursomc.domain.Pedido;
 import com.danielyoshio.cursomc.repositories.PedidoRepository;
 import com.danielyoshio.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -17,7 +17,7 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
